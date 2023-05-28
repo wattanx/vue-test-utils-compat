8d9594beda2f1a987de75de18f46cafae4750798
wattanx <wattan.dev@gmail.com>
2023-05-28 15:22:11 +0900

    feat: props transform

--- packages/vite-plugin-vue-test-utils-compat/src/index.ts
+++ packages/vite-plugin-vue-test-utils-compat/src/index.ts
@@ -1,4 +1,8 @@
 import type { Plugin } from "vite";
+import { walk } from "estree-walker";
+import type { Identifier } from "estree";
+import type { AcornNode } from "rollup";
+import MagicString from "magic-string";
 
 const compatNames = ["mount", "shallowMount"];
 
@@ -20,7 +24,46 @@ function VueTestUtilsCompatPlugin(): Plugin {
       return;
     }
 
-    return code;
+    const s = new MagicString(code);
+
+    walk(ast as any, {
+      enter(node) {
+        if (
+          node.type === "ImportDeclaration" &&
+          node.source.value === "@wattanx/vite-plugin-vue-test-utils-compat"
+        ) {
+          const source = node.source;
+          s.overwrite(source.range![0], source.range![1], "'@vue/test-utils'");
+        }
+
+        // props -> propsData
+        if (
+          node.type === "CallExpression" &&
+          node.callee.type === "Identifier" &&
+          compatNames.includes(node.callee.name)
+        ) {
+          const argument = node.arguments[1];
+          if (argument.type === "ObjectExpression") {
+            const properties = argument.properties;
+            const propsData = properties.find(
+              (p) =>
+                p.type === "Property" &&
+                p.key.type === "Identifier" &&
+                p.key.name === "props"
+            );
+            if (propsData?.type === "Property") {
+              const key = propsData.key as Identifier;
+              s.overwrite(key.range![0], key.range![1], "propsData");
+            }
+          }
+        }
+      },
+    });
+
+    return {
+      code: s.toString(),
+      map: s.generateMap(),
+    };
   }
 
   return {
